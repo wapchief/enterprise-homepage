42ca83ef1a5c037c2161387c5c78f47f19708a01
吴冰 <wapchief@qq.com>
2021-06-28 15:35:33 +0800

    init

---
+++ src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div>header</div>
+    <img src="../assets/footer.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Header to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  height: 70px;
+}
+</style>
